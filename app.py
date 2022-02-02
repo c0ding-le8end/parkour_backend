d8cd1cfc0eae37884f08c5bcecc7db0b8c5bf343
c0ding-le8end <hamdaan.a.sawar@gmail.com>
2022-02-02 17:28:58 +0530

    changes made to procfile

--- app.py
+++ app.py
@@ -313,7 +313,8 @@ def shutdown_session(exception=None):
 @app.before_request
 def main():
     init_db()
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
  # app.run(host='127.0.0.1', port=5000, debug=True)
 
 
