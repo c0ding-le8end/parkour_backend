676419a5563e179782ce22da162dd667d1e75544
c0ding-le8end <hamdaan.a.sawar@gmail.com>
2022-02-03 18:01:05 +0530

    production deployment

--- app.py
+++ app.py
@@ -310,11 +310,8 @@ def logout():
 def shutdown_session(exception=None):
     db.remove()
 
-@app.before_request
-def main():
-    init_db()
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
  # app.run(host='127.0.0.1', port=5000, debug=True)
 
 
